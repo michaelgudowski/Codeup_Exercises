95b3d83a439a584a966fd6f67f3b752205484e6f
Michael Gudowski <michaelgudowski@gmail.com>
2014-11-21 11:21:44 -0600

    update on if.php (adding if else statements)

--- if.php
+++ if.php
@@ -1,47 +1,42 @@
 <?php
 
-$a = 5;
-$b = 10;
-$c = '10';
+$a = 11; $b = 11.1; $c = -5;
 
+// Shorten the next 2 statements into an if/else
 if ($a < $b) {
     // output the appropriate result
     echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
+} elseif ($a > $b) {
     // output the appropriate result
-    echo "$b is greater $a\n";
+    echo "$a is greater than $b\n";
 }
 
+// Shorten the next 2 statements into an if/else
 if ($b >= $c) {
     // output the appropriate result
     echo "$b is greater than or equal to $c\n";
 }
-
-if ($b <= $c) {
+elseif ($b <= $c) {
     // output the appropriate result
     echo "$b is less than or equal to $c\n";
 }
 
+// combine the next 4 conditionals into
+// one if/else/elseif block that checks in order for:
+// identical, equal, not identical, not equal
 if ($b == $c) {
     // output the appropriate result
     echo "$b is equal to $c\n";
 }
-
-if ($b === $c) {
+elseif ($b === $c) {
     // output the appropriate result
     echo "$b is identical to $c\n";
 }
 
-// output the appropriate result
-if ($b != $c) {
+elseif ($b != $c) {
+    // output the appropriate result
     echo "$b is not equal to $c\n";
-}
-
-// output the appropriate result
-if ($b !== $c) {
+} else {
+    // output the appropriate result
     echo "$b is not identical to $c\n";
 }
-
-?>
