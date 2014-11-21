3630fb4b87315a86801896338008d5ada786941f
Michael Gudowski <michaelgudowski@gmail.com>
2014-11-21 13:54:29 -0600

    final edit

--- do_while.php
+++ do_while.php
@@ -7,3 +7,16 @@ do {
 } while ($a <= 100);
 
 
+$b = 100;
+
+do { 
+	echo "$b \n";
+	$b = $b - 5;
+} while ($b >= -10);
+
+
+$c = 2;
+ do {
+ 	echo "$c \n";
+ 	$c = $c * $c;
+ } while ($c <= 1000000);
