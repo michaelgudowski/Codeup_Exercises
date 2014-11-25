33a459450fe48bb4ac9e6c9cac8a4d2f33de33ad
Michael Gudowski <michaelgudowski@gmail.com>
2014-11-25 15:56:59 -0600

    With break

--- break_continue.php
+++ break_continue.php
@@ -1,9 +1,18 @@
 <?php
-
+//label even numbers and continue for loop.
 for ($number = 1; $number <= 100; $number++) {
     echo $number . "\n";
     if ($number % 2 !== 0) {
         continue;
     }
     echo "^ that is an even number.\n";
+}
+
+//break after hitting 10.
+
+for ($number = 0; $number <= 100; $number++) {
+	echo $number . "\n";
+	if ($number == 10) {
+		break;
+	}
 }
