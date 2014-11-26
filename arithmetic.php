ff729564f3d8ab82831d8ef14241c0a6e854c97d
Michael Gudowski <michaelgudowski@gmail.com>
2014-11-26 15:54:16 -0600

    Functions with Arguments

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,60 @@
 <?php
 
-function add($a, $b) {
-    echo $a + $b;
+function divideby0error($a,$b)
+{
+	if ($b === 0){
+		echo "Error: Attempting to divide by 0";
 }
 
-function subtract($a, $b) {
-    echo $a - $b;
+
+
+
+function add($a, $b) 
+{
+	if (is_numeric($a) && is_numeric($b)) {
+        echo $a + $b;
+    } else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-function multiply($a, $b) {
-    echo $a * $b;
+function subtract($a, $b) 
+{
+	if (is_numeric($a) && is_numeric($b)) {   
+    	echo $a - $b;
+	}	else {
+		  echo "Error: Both arguments must be numbers\n";
+	}
 }
 
-function divide($a, $b) {
-    echo $a / $b;
+function multiply($a, $b)
+{
+	if (is_numeric($a) && is_numeric($b)) {	
+		echo $a * $b;
+	}	else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }       
 }
 
-function modulus($a, $b) {
-    echo $a % $b;
+
+function divide($a, $b)
+{
+	fuction divideby0error($a,$b)
+	}	
+	elseif (is_numeric($a) && is_numeric($b)) {	
+		echo $a / $b;
+	}	else {
+        echo "ERROR: Both arguments must be numbers. You entered ($a) ($b)\n";
+    }      
+}
+
+function modulus($a, $b)
+{
+	if (is_numeric($a) && is_numeric($b)) {	
+		echo $a % $b;
+	}	else {
+        echo "ERROR: Both arguments must be numbers\n";
+    	}       
 }
 
 echo add(1,8) . PHP_EOL;
@@ -26,8 +63,6 @@ echo subtract(100,90) . PHP_EOL;
 
 echo multiply(2,10) . PHP_EOL;
 
-echo divide(40,4) . PHP_EOL;
+echo divide(10,'r') . PHP_EOL;
 
-echo modulus(10,5) . PHP_EOL;
-
-?>
+echo modulus(10, 5) . PHP_EOL;
