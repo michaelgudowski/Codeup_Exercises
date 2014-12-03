696e1e06d54db8c3815e5a7b49b11296889e1519
Michael Gudowski <michaelgudowski@gmail.com>
2014-12-03 11:44:11 -0600

    List with commas using functions

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,19 +1,46 @@
 <?php
 
-$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+// $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$famousFakePhysicists = explode(', ',$physicistsString);
+// $famousFakePhysicists = explode(', ',$physicistsString);
 
-$lastString = array_pop($famousFakePhysicists);
+// $lastString = array_pop($famousFakePhysicists);
 
-array_push($famousFakePhysicists, 'and');
+// array_push($famousFakePhysicists, 'and');
 
-array_push($famousFakePhysicists, $lastString);
+// array_push($famousFakePhysicists, $lastString);
 
-$finalString = implode(', ', $famousFakePhysicists);
+// $finalString = implode(', ', $famousFakePhysicists);
 
-echo "$finalString\n";
+// echo "Some of the most famous fictional theoretical physicists are $finalString\n";
 
-//print_r($famousFakePhysicists);
+// print_r($famousFakePhysicists);
 
-//echo "Some of the most famous fictional theoretical physicists are $famousFakePhysicists";
+// echo "Some of the most famous fictional theoretical physicists are $famousFakePhysicists";
+
+
+
+
+function humanizedList($string, $alpha=false) {
+	$array = explode(', ',$string);
+
+	if ($alpha){
+		sort($array);
+
+	}
+
+	$lastString = array_pop($array);
+
+    $finalString = implode(', ', $array);
+    return $finalString . ', and ' . $lastString;
+
+}
+ // List of famous peeps
+ $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+ 
+ // Humanize that list
+ $famousFakePhysicists = humanizedlist($physicistsString, true);
+
+ // Output sentence
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
